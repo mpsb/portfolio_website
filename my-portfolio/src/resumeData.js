79e8ccc5fe2151c153f494ba6549ff1fe84113b2
mpsb <mps.bilo@gmail.com>
2020-12-07 21:10:18 +0800

    added about component

---
+++ my-portfolio/src/resumeData.js
@@ -0,0 +1,6 @@
+let resumeData = {
+  aboutme:
+    "I am currently an Analyst Programmer working in Hong Kong International School. I'm an aspiring data and software engineer, able to build user-friendly, full stack applications that are scalable with huge amounts of data. During my free time, I'm working on side projects while preparing my application for a Masters in Computer Science. As such, I'm also currently taking two part-time university courses online; Python Programming Fundamentals from UCSDE and Calculus I from the University of Pittsburgh.",
+};
+
+export default resumeData
