d557abb1f7bb723e09c88fe884565e226125e796
mpsb <mps.bilo@gmail.com>
2020-12-08 18:30:32 +0800

    added css and js for ui

--- my-portfolio/src/components/header.js
+++ my-portfolio/src/components/header.js
@@ -6,7 +6,7 @@ export default class Header extends Component {
 
     return (
       <React.Fragment>
-        <header id="home">
+        <header id="home" className="App-header">
           <nav id="nav-wrap">
             <a className="mobile-btn" href="#nav-wrap" title="Show navigation">
               Show navigation
