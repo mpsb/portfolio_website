33e163a313a837d7f0efc6b5cabcf8f6259c3c09
mpsb <mps.bilo@gmail.com>
2020-12-08 22:09:49 +0800

    edited about component and updated favicon

--- my-portfolio/src/components/about.js
+++ my-portfolio/src/components/about.js
@@ -7,17 +7,9 @@ export default class About extends Component {
     return (
       <section id="about">
         <div className="row">
-          <div className="three columns">
-            <img className="profile-pic" src="images/profile.jpg" alt="" />
-          </div>
-          <div className="nine columns main-col">
+          <div className="twelve columns main-col">
             <h2>About Me</h2>
             <p>{resumeData.aboutme}</p>
-            <div className="row">
-              <div className="columns contact-details">
-                <h2>Contact Details</h2>
-              </div>
-            </div>
           </div>
         </div>
       </section>
