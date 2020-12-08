ea0e82f11988c88a0e55db33eab6169014aafd90
mpsb <mps.bilo@gmail.com>
2020-12-09 00:54:21 +0800

    added login form and css changes to input

--- my-portfolio/src/components/login.js
+++ my-portfolio/src/components/login.js
@@ -8,7 +8,22 @@ export default class Login extends Component {
         <div className="row">
           <div className="twelve columns main-col">
             <h2>Login</h2>
-          </div>
+            </div>
+        </div>
+        <div className="row">
+          <div className="twelve columns main-col">
+              <form>
+              <div className="form-group">
+                <label>Username</label>
+                <input type="text" className="form-control" placeholder="Username"/>
+            </div>
+            <div className="form-group">
+                <label>Password</label>
+                <input type="password" className="form-control" placeholder="Password"/>
+            </div>
+            <button type="submit" className="btn btn-primary btn-block">Login</button>
+              </form>
+              </div>
         </div>
       </section>
     );
