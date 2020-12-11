81481f9b889f255e738ab9903e54fcde2e8a3ab3
mpsb <mps.bilo@gmail.com>
2020-12-11 23:29:27 +0800

    reformatted utils.js, proper function separation

--- backend/utils.js
+++ backend/utils.js
@@ -1,28 +1,28 @@
 // generate token using secret from process.env_JWT_SECRET
-var jwt = require('jsonwebtoken');
+var jwt = require("jsonwebtoken");
 
 // generate token and return
 function generateToken(user) {
-    if (!user) {
-        return null;
-    }
+  if (!user) {
+    return null;
+  }
 
-    var u = {
-        userId: user.userId,
-        name: user.name,
-        username: user.username,
-        isAdmin: user.isAdmin
-    };
+  var u = {
+    userId: user.userId,
+    name: user.name,
+    username: user.username,
+    isAdmin: user.isAdmin,
+  };
 
-    // signing authentication token
-    return jwt.sign(u, process.env.JWT_SECRET, {
-        expiresIn: 60*60*24
-    });
+  // signing authentication token
+  return jwt.sign(u, process.env.JWT_SECRET, {
+    expiresIn: 60 * 60 * 24,
+  });
+}
 
-    // return basic user details
-    function getCleanUser(user) {
-        if (!user) {
-            return null;
-        }
-    }
+// return basic user details
+function getCleanUser(user) {
+  if (!user) {
+    return null;
+  }
 }
