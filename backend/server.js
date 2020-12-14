09ef6452c01703c16ec90055fef8746a7aaffeb4
mpsb <mps.bilo@gmail.com>
2020-12-14 19:39:15 +0800

    added middleware to validate token

--- backend/server.js
+++ backend/server.js
@@ -27,8 +27,37 @@ app.use(bodyParser.json());
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// middleware that checks if JWT exists and verifies if it doesn't.
+// in all future routes, this helps to know if the request is authenticated or not.
+app.use(function (req, res, next) {
+    // check header or url parameters or post parameters for token
+    var token = req.headers['authorization'];
+    if (!token) {
+        return next(); // if no token, continue
+    }
+
+    token = token.replace('Bearer ', '');
+    jwt.verify(token, process.env.JWT_SECRET, function (err, user) {
+        if (err) {
+            return res.status(401).json({
+                error: true,
+                message: "Invalid user."
+            });
+        } else {
+            req.user = user; // set user to req so other routes can use it
+            next();
+        }
+    });
+});
+
 //request handlers
 app.get('/', (req, res) => {
+    if (!req.user) {
+        return res.status(401).json({
+            success: false,
+            message: 'Invalid user to access it.'
+        });
+    }
     res.send('Welcome to the Node.js Tutorial! - ' + req.user.name);
 });
 
