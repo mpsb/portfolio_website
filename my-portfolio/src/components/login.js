aaa0405d821f598a7c446d6b0bb127e00aa4ce81
mpsb <mps.bilo@gmail.com>
2020-12-16 20:54:18 +0800

    changed login component into function

--- my-portfolio/src/components/login.js
+++ my-portfolio/src/components/login.js
@@ -1,31 +1,71 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
-export default class Login extends Component {
-  render() {
+function Login(props) {
+  const username = useFormInput("");
+  const password = useFormInput("");
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
 
-    return (
-      <section id="login">
-        <div className="row">
-          <div className="twelve columns main-col">
-            <h2>Login</h2>
-            </div>
+  // handle button click of login form
+  const handleLogin = () => {
+    props.history.push("./dashboard");
+  };
+
+  return (
+    <section id="login">
+      <div className="row">
+        <div className="twelve columns main-col">
+          <h2>Login</h2>
         </div>
-        <div className="row">
-          <div className="twelve columns main-col">
-              <form>
-              <div className="form-group">
-                <label>Username</label>
-                <input type="text" className="form-control" placeholder="Username"/>
+      </div>
+      <div className="row">
+        <div className="twelve columns main-col">
+          <form>
+            <div className="form-group">
+              <label>Username</label>
+              <input
+                type="text"
+                {...username}
+                autoComplete="new-password"
+                className="form-control"
+                placeholder="Username"
+              />
             </div>
             <div className="form-group">
-                <label>Password</label>
-                <input type="password" className="form-control" placeholder="Password"/>
+              <label>Password</label>
+              <input
+                type="password"
+                {...password}
+                autoComplete="new-password"
+                className="form-control"
+                placeholder="Password"
+              />
             </div>
-            <button type="submit" className="btn btn-primary btn-block">Login</button>
-              </form>
-              </div>
+            <input
+              type="button"
+              value={loading ? "Loading..." : "Login"}
+              onClick={handleLogin}
+              disabled={loading}
+              className="btn btn-primary btn-block"
+            />
+          </form>
         </div>
-      </section>
-    );
+      </div>
+    </section>
+  );
+}
+
+const useFormInput = initialValue => {
+  const [value, setValue] = useState(initialValue);
+
+  const handleChange = e => {
+    setValue(e.target.value);
+  }
+
+  return {
+    value,
+    onChange: handleChange
   }
 }
+
+export default Login;
