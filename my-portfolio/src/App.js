1a9a2046e0ef9d15dc3d69f260563ab26d59f818
mpsb <mps.bilo@gmail.com>
2020-12-17 21:20:37 +0800

    converted App to function, adding BrowserRouter for paths

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,22 +1,28 @@
 import React, { Component } from "react";
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import About from "./components/about";
 import Header from "./components/header";
 import Login from "./components/login";
 import Projects from "./components/projects";
+import Dashboard from "./components/dashboard";
 import resumeData from "./resumeData";
 import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
         <Header resumeData={resumeData} />
         <About resumeData={resumeData} />
-        <Projects resumeData = {resumeData} />
-        <Login resumeData={resumeData}/>
-      </div>
-    );
-  }
+        <Projects resumeData={resumeData} />
+        <Login resumeData={resumeData} />
+        <Switch>
+          <Route path='/login' component={Login} />
+          <Route path='/dashboard' component={Dashboard}/>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
